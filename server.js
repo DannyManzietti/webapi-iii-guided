f241c78c8093ed109bcc90e3fde9617f91fd199a
DannyManzietti <danmanzietti@gmail.com>
2019-10-30 13:30:35 -0400

    ...

--- server.js
+++ server.js
@@ -15,9 +15,22 @@ const httpLogger = (req, res, next) => {
   next();
 };
 
-server.use(express.json());
+function gateKeeper(req, res, next) {
+  // data can come in the body, url parameters, query string, headers
+  // new way of reading data sent by the client
+  const password = req.headers.password || "";
+  if (!password) {
+    res.status(401).json({ message: "Oh Yeah, put that password in homie" });
+  } else {
+    password.toLowerCase() === "mellon"
+      ? next()
+      : res.status(400).json({ message: "ok ok ok" });
+  }
+}
 server.use(helmet());
-server.use(dateLogger);
+server.use(express.json());
+server.use(gateKeeper);
+// server.use(dateLogger);
 server.use(httpLogger);
 
 server.use("/api/hubs", hubsRouter);
